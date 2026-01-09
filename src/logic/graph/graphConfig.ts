bb0d8336b81ebdb0c0cd5526b48898774ce2e8c8
Espen Erlandsen <espen.erlandsen@gmail.com>
2026-01-09 19:42:50 +0100

    Adjust tier colors

--- src/logic/graph/graphConfig.ts
+++ src/logic/graph/graphConfig.ts
@@ -76,23 +76,30 @@ export const stylesheet: any[] = [
     {
         selector: '.tier-1',
         style: {
-            'background-color': '#f97316',
-            'border-color': '#fdba74',
+            'background-color': '#CB854D',
+            'border-color': '#E2B186',
             'font-weight': 'bold',
         },
     },
     {
         selector: '.tier-2',
         style: {
-            'background-color': '#0ea5e9',
-            'border-color': '#7dd3fc',
+            'background-color': '#607D8B',
+            'border-color': '#9FB0B8',
         },
     },
     {
         selector: '.tier-3',
         style: {
-            'background-color': '#10b981',
-            'border-color': '#6ee7b7',
+            'background-color': '#8A9A5B',
+            'border-color': '#C0CBA0',
+        },
+    },
+    {
+        selector: '.tier-4',
+        style: {
+            'background-color': '#DBC6A4',
+            'border-color': '#E8D9C2',
         },
     },
     {
